67d80ac765fda5e8aa22ce90710d74ba4ac8b626
shakyracc <shakyracornelius@gmail.com>
2025-04-21 09:31:43 -0400

    tidy up

--- lab2.py
+++ lab2.py
@@ -7,13 +7,19 @@ class Account:
         self.account_number = account_number
         self.account_balance = account_balance
         self.account_name = ""
-        account_count = account_count + 1
+        Account.account_count += 1
+
+    @classmethod
+    def check_account_count(cls):
+        return cls.account_count
 
     def withdraw(self, amount):
         print("Withdrawing money")
-    
+        self.account_balance -= amount
+
     def deposit(self, amount):
         print("Depositing money")
+        self.account_balance += amount
 
     def check_balance(self):
         print(f"Your balance is: {self.account_balance}")
@@ -25,22 +31,14 @@ class Account:
     def change_name(self, new_name):
         self.account_name = new_name
 
-    '''
-    Add a function **accountConsolidate(Account acct1, Account acct2)**
-    to your **Account** class that creates a new account whose balance is the sum
-    of the balances in **acct1** and **acct2** and closes **acct1** and **acct2**. The new
-    account should be returned. Two important rules of consolidation:
-    - (a) Only accounts with the same name can be consolidated. The new
-    account gets the name on the old accounts but a new account number.
-    - (b) Two accounts with the same number cannot be consolidated. Otherwise, this would be an easy way to double your money!
-    Check these conditions before creating the new account. If either condition
-    fails, do not create the new account or close the old ones; print a useful
-    message and return **None**.
-    '''
-
     def close_account(self):
-        self.account_name = self.account_name + "_CLOSED"
-        self.account_balance = 0
+        if not self.account_name.endswith("_CLOSED"):
+            self.account_name = self.account_name + "_CLOSED"
+            self.account_balance = 0
+            Account.account_count -= 1
+            print("Account closed")
+        else:
+            print("Account already closed.")
         print(self.account_name)
         print(self.account_balance)
 
@@ -51,14 +49,11 @@ class Account:
             new_account.change_name(acct1.account_name)
             acct1.close_account()
             acct2.close_account()
+            print("Account consolidated!")
             return new_account
         else:
             print("Accounts cannot be consolidated.")
             return None
-
-
-#  Once an ID is accepted, the main menu is displayed as shown in the sample run. You can enter choice 1 for viewing the current balance, 2 for withdrawing money, 3 for depositing money, and 4 for exiting the main menu. Once you exit, the system will prompt for an id again. Thus, once the system starts, it will not stop.
-
 
 def display_menu(acc):
     print(acc.account_number)
@@ -77,23 +72,10 @@ def display_menu(acc):
     else: 
         print("Enter a valid option")
  
-        
-
-# Create 10 accounts in a list with ID 0, 1,...,9, and an initial balance of $100
-
 accounts_list = [Account(str(i), 100) for i in range(10)]
 
-#print("Print initial accounts with $100 balance:")
-
-#for acc in accounts_list:
-#    print(f"ID: {acc.account_number}, Balance: {acc.account_balance}")
-
-# Prompt the user to enter an ID.
-
 def is_valid_ID(user_id):
-    # print(user_id)
     for acc in accounts_list:
-        # print(acc.account_number)
         if acc.account_number == user_id:
             print(accounts_list[int(user_id)])
             return acc
@@ -115,7 +97,7 @@ def generate_random_account_number():
 
     for acc in accounts_list:
         if acc.account_number == account_number:
-            generate_random_account_number()
+            return generate_random_account_number()
     return account_number
 
 ID_prompt()
